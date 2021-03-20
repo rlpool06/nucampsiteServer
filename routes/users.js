31f5547f872b500b5234a9444af49df9e53b1644
Ryan Pool <rlpool06@gmail.com>
2021-03-20 09:23:27 -0500

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
+const aunthenticate = require('../authenticate');
+const { authenticate } = require('../models/user');
 
 const router = express.Router();
 
@@ -14,16 +16,30 @@ router.post('/signup', (req, res) => {
   User.register(
       new User({username: req.body.username}),
       req.body.password,
-      err => {
+      (err, user) => {
           if (err) {
               res.statusCode = 500;
               res.setHeader('Content-Type', 'application/json');
               res.json({err: err});
           } else {
-              passport.authenticate('local')(req, res, () => {
-                  res.statusCode = 200;
-                  res.setHeader('Content-Type', 'application/json');
-                  res.json({success: true, status: 'Registration Successful!'});
+              if (req.body.firstname) {
+                  user.firstname = req.body.firstname;
+              }
+              if (req.body.lastname) {
+                  user.lastname = req.body.lastname;
+              }
+              user.save(err => {
+                  if (err) {
+                      res.statusCode = 500;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json({err: err});
+                      return;
+                  }
+                  passport.authenticate('local')(req, res, () => {
+                      res.statusCode = 200;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json({success: true, status: 'Registration Successful!'});
+                  });
               });
           }
       }
@@ -31,6 +47,7 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, status: 'You are successfully logged in!'});
